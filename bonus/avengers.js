5af2b986818cfb564368173f740597b25cd67b2e
sean3z <seanwragg@gmail.com>
2015-12-24 18:41:25 -0500

    fix spacing

--- bonus/avengers.js
+++ bonus/avengers.js
@@ -1,19 +1,19 @@
 // storage of avengers
 module.exports = {
-	Thor: {
-		identity: 'Jane Foster',
-		location: 'Asgard'
-	},
-	'Captain America': {
-		identity: 'Sam Wilson',
-		location: 'New York City, New York'
-	},
-	'Iron Man': {
-		identity: 'Anthony Stark',
-		location: 'Long Island, New York'
-	},
-	'Spider-Man': {
-		identity: 'Miles Morales',
-		location: 'Manhattan, New York'
-	}
+  Thor: {
+    identity: 'Jane Foster',
+    location: 'Asgard'
+  },
+  'Captain America': {
+    identity: 'Sam Wilson',
+    location: 'New York City, New York'
+  },
+  'Iron Man': {
+    identity: 'Anthony Stark',
+    location: 'Long Island, New York'
+  },
+  'Spider-Man': {
+    identity: 'Miles Morales',
+    location: 'Manhattan, New York'
+  }
 };
--- bonus/index.js
+++ bonus/index.js
@@ -8,42 +8,42 @@ var avengers = require('./avengers');
 
 // return information about the avenger
 app.get('/avenger/:hero', function(req, res) {
-	res.send(avengers[req.params.hero]);
+  res.send(avengers[req.params.hero]);
 });
 
 // update information about the avenger
 app.post('/avenger/:hero', function(req, res) {
-	if (!avengers[req.params.hero]) {
-		res.send(400);
-		return;
-	}
+  if (!avengers[req.params.hero]) {
+    res.send(400);
+    return;
+  }
 
-	avengers[req.params.hero] = req.body;
-	res.send(200);
+  avengers[req.params.hero] = req.body;
+  res.send(200);
 });
 
 // create new avenger
 app.put('/avenger/:hero', function(req, res) {
-	if (avengers[req.params.hero]) {
-		res.send(400);
-		return;
-	}
+  if (avengers[req.params.hero]) {
+    res.send(400);
+    return;
+  }
 
-	avengers[req.params.hero] = req.body;
-	res.send(200);
+  avengers[req.params.hero] = req.body;
+  res.send(200);
 });
 
 // delete existing avenger
 app.delete('/avenger/:hero', function(req, res) {
-	if (avengers[req.params.hero]) {
-		res.send(400);
-		return;
-	}
+  if (avengers[req.params.hero]) {
+    res.send(400);
+    return;
+  }
 
-	avengers[req.params.hero] = null;
-	res.send(200);
+  avengers[req.params.hero] = null;
+  res.send(200);
 });
 
 app.listen(8080, function() {
-	console.log('Server started on http://localhost:8080');
+  console.log('Server started on http://localhost:8080');
 });
